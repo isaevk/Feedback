02357abbad097dbf98f3d7de53f8c38345ce022e
isaevk <kirill_isaev@aol.com>
2023-07-24 13:44:10 +0300

    Initial Commit

---
+++ Feedback/FeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedbackApp.swift
+//  Feedback
+//
+//  Created by Kirill Dev on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
